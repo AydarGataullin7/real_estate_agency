79cfc0c5d0d0321673fb3fbaebf618099841d81f
AydarGataullin7 <aydarcr7@gmail.com>
2026-03-17 14:46:24 +0500

    add owners inline to flat admin page

--- property/admin.py
+++ property/admin.py
@@ -3,6 +3,11 @@ from django.contrib import admin
 from .models import Flat, Complaint, Owner
 
 
+class FlatAndOwnerInline(admin.TabularInline):
+    raw_id_fields = ['owner']
+    model = Flat.owners.through
+
+
 class FlatAdmin(admin.ModelAdmin):
     search_fields = ('town', 'address', 'owner')
     readonly_fields = ['created_at']
@@ -11,6 +16,7 @@ class FlatAdmin(admin.ModelAdmin):
     list_editable = ['new_building']
     list_filter = ['new_building', 'rooms_number', 'has_balcony']
     raw_id_fields = ['liked_by']
+    inlines = [FlatAndOwnerInline]
 
 
 class ComplaintAdmin(admin.ModelAdmin):
@@ -21,6 +27,7 @@ class ComplaintAdmin(admin.ModelAdmin):
 class OwnerAdmin(admin.ModelAdmin):
     list_display = ['owner']
     raw_id_fields = ['flats']
+    search_fields = ['owner']
 
 
 admin.site.register(Flat, FlatAdmin)
