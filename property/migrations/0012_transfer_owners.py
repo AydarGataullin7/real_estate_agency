ad2544b8f3a84fe2eb1d8e59a89eaf31d5c602ec
AydarGataullin7 <aydarcr7@gmail.com>
2026-03-17 12:18:30 +0500

    migrate owners data to new model

---
+++ property/migrations/0012_transfer_owners.py
@@ -0,0 +1,29 @@
+from django.db import migrations
+
+
+def transfer_owners(apps, schema_editor):
+    Flat = apps.get_model('property', 'Flat')
+    Owner = apps.get_model('property', 'Owner')
+
+    for flat in Flat.objects.all():
+        owner, created = Owner.objects.get_or_create(owner=flat.owner, defaults={
+            'owners_phonenumber': flat.owners_phonenumber,
+            'owner_pure_phone': flat.owner_pure_phone,
+        }
+        )
+
+
+def reverse_transfer(apps, schema_editor):
+    Owner = apps.get_model('property', 'Owner')
+    Owner.objects.all().delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('property', '0011_owner_owner_pure_phone_owner_owners_phonenumber'),
+    ]
+
+    operations = [
+        migrations.RunPython(transfer_owners, reverse_transfer),
+    ]
